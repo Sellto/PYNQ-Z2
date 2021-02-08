bb1043262d92c7664b2f5dc3e6eb509ede468916
sellto <tom.selleslagh@gmail.com>
2021-02-08 13:23:31 +0100

    update package data

--- setup.py
+++ setup.py
@@ -18,8 +18,7 @@ setup(
 	include_package_data = True,
 	packages=find_packages(),
 	package_data = {
-	'' : ['*.py'],
-	'bitstreams': ['*.bit','*.tcl'],
+	'' : ['bitstreams/*.bit','bitstreams/*.tcl','*.py'],
 	},
 	data_files = [(os.path.join('/home/xilinx/jupyter_notebooks/SD4_Lab2',root.replace('notebooks/','')), [os.path.join(root, f) for f in files]) for root, dirs, files in os.walk('notebooks/')],
 	description = "PYNQ-Z2 labs using a hardware accelerated "
